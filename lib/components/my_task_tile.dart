546e21292e0c015be2d43e707a81ca5d4ed225f5
carlstep <carlstep333@me.com>
2024-05-17 17:14:13 +0700

    removed MyTaskTile container

--- lib/components/my_task_tile.dart
+++ lib/components/my_task_tile.dart
@@ -35,82 +35,80 @@ class _MyTaskTileState extends State<MyTaskTile> {
           _isExpanded = !_isExpanded;
         });
       },
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.primary,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: const EdgeInsets.only(top: 10, left: 25, right: 25),
-        child: Card(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // taskName - settings for displaying task name
-              Padding(
-                padding: const EdgeInsets.only(left: 10, top: 0, right: 0),
-                child: Row(
-                  children: [
-                    Text(
-                      widget.task.taskName,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        decoration: widget.task.isComplete
-                            ? TextDecoration.lineThrough
-                            : TextDecoration.none,
-                      ),
+      child: Card(
+        elevation: 0,
+        color: widget.task.isComplete
+            ? Colors.green.shade200
+            : Theme.of(context).colorScheme.primary,
+        margin: const EdgeInsets.only(left: 20, top: 10, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // taskName - settings for displaying task name
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 0, right: 0),
+              child: Row(
+                children: [
+                  Text(
+                    widget.task.taskName,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      decoration: widget.task.isComplete
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
                     ),
-                    const Expanded(
-                      child: SizedBox(
-                        width: 20,
-                      ),
+                  ),
+                  const Expanded(
+                    child: SizedBox(
+                      width: 20,
                     ),
-                    // CheckBox
-                    // value = bool value, onChanged allows to switch between true/false
-                    Checkbox(
-                      activeColor: Colors.green,
-                      value: widget.task.isComplete,
-                      onChanged: widget.onChanged,
-                    ),
-                  ],
-                ),
+                  ),
+                  // CheckBox
+                  // value = bool value, onChanged allows to switch between true/false
+                  Checkbox(
+                    activeColor: Colors.green,
+                    value: widget.task.isComplete,
+                    onChanged: widget.onChanged,
+                  ),
+                ],
               ),
-              // taskNote - settings for displaying task description
-              Padding(
-                padding: const EdgeInsets.only(left: 10, top: 0, right: 0),
-                child: _isExpanded
-                    ? Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text(widget.task.taskNote),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const Text('11/06/2024'),
-                              const Expanded(
-                                child: SizedBox(
-                                  width: 20,
-                                ),
+            ),
+            // taskNote - settings for displaying task description
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 0, right: 0),
+              child: _isExpanded
+                  ? Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(widget.task.taskNote),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Text('11/06/2024'),
+                            const Expanded(
+                              child: SizedBox(
+                                width: 20,
                               ),
-                              IconButton(
-                                onPressed: widget.editFunction,
-                                icon: const Icon(Icons.settings),
-                              ),
-                              IconButton(
-                                onPressed: widget.deleteFunction,
-                                icon: const Icon(Icons.delete),
-                              ),
-                            ],
-                          )
-                        ],
-                      )
-                    : null,
-              ),
-            ],
-          ),
+                            ),
+                            IconButton(
+                              onPressed: widget.editFunction,
+                              icon: const Icon(Icons.settings),
+                            ),
+                            IconButton(
+                              onPressed: widget.deleteFunction,
+                              icon: const Icon(Icons.delete),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  : null,
+            ),
+          ],
         ),
       ),
     );
